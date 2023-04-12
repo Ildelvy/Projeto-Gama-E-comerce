8b77e5fc415962cd1b74963f868d18f2c112f027
Gustavo Rodrigues <77814906+Gusdevr@users.noreply.github.com>
2023-04-12 18:10:32 -0300

    Inclusão das novas rotas
    Adição das rotas Login e Cadastro.

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes  } from 'react-router-dom'
 import  Home  from './pages/Home'
 import  Login  from './pages/Login'
+import Cadastro from './pages/Cadastro'
+import Loja from './pages/Loja'
 
 
 function Router() {
@@ -11,7 +13,8 @@ return (
 
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
-
+        <Route path='/cadastro' element={<Cadastro />} />
+        <Route path='/loja' element={<Loja />} />
     </Routes>
     </BrowserRouter>
 )
